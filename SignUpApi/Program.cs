e1c44187f4c8ad511c67000daa161be1af7253c3
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-09-27 22:40:16 +0330

    add Config  Swagger in program.cs

--- SignUpApi/Program.cs
+++ SignUpApi/Program.cs
@@ -6,10 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(Options =>
     Options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
