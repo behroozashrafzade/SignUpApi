23a6f9dbda28f08316e18670f945bd1c07495dcf
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-09-27 22:35:11 +0330

    Implementation  get ,post in OperatorController

---
+++ SignUpApi/Controllers/OperatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignUpApi.EFCore;
+using SignUpApi.Model;
+
+namespace SignUpApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperatorController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public OperatorController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Operator>>> GetOprators()
+        {
+            return await _context.Operators.ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Operator>> PostOperator(Operator op)
+        {
+            _context.Operators.Add(op);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetOperator", new {id=op.Id},op);
+        }
+
+    }
+}
